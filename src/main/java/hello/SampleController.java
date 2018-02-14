fbc24d0880323b9f02630ac66fd114d0814f488e
Mendoza <menpedro@amazon.com>
2018-02-14 11:53:05 +0100

    adding docker support

--- src/main/java/hello/SampleController.java
+++ src/main/java/hello/SampleController.java
@@ -5,14 +5,19 @@ import org.springframework.boot.autoconfigure.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @Controller
 @EnableAutoConfiguration
 public class SampleController {
+    DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 
     @RequestMapping("/")
     @ResponseBody
     String home() {
-        return "Hello World!";
+        return "Hello World!. Current time is: " + dateFormat.format(new Date());
     }
 
     public static void main(String[] args) throws Exception {
